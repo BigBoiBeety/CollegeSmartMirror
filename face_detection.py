d74d10a3fe4046b72af268480a105bd6373043a9
Lewis "Big Boi" Beety <16beetyl@utcsheffield.org.uk>
2022-07-22 09:16:56 +0100

    FInd faces

--- face_detection.py
+++ face_detection.py
@@ -6,6 +6,12 @@ camera = cv.VideoCapture(0) # Get default video capture
 def mainLoop(): # Start the main display loop
     while True:
         ret, frame = camera.read() # Get the current frame
+
+        face_locations = fr.face_locations(frame)
+
+        for (top, right, bottom, left) in face_locations:
+            cv.rectangle(frame, (left, top), (right, bottom), (0,0,255), cv.FILLED)
+
         cv.imshow('Video', frame) # Display the frame in a window
 
         if cv.waitKey(1) & 0xFF == ord('q'): # If q is pressed, end the loop
