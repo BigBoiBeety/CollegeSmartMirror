db2019cbd0d238336244de6a59667e369a7312c3
Lewis "Big Boi" Beety <16beetyl@utcsheffield.org.uk>
2022-10-10 15:35:27 +0100

    Added a bunch of methods

--- database.py
+++ database.py
@@ -31,6 +31,37 @@ def InsertCollegeUpdate(collegeUpdateClass):
     conn.commit()
     return cursor.lastrowid
 
+
+def InsertAssignment(assignmentClass, userID):
+    # Validation
+    if type(assignmentClass) is not Assignment:
+        raise TypeError("Not a assignment class")
+    
+    sqlcode = """INSERT INTO assignments(subject, teacher, date, userID) VALUES (?,?,?,?)"""
+    data = (assignmentClass.subject, assignmentClass.teacher, assignmentClass.dueDate, userID)
+
+    conn = connectToDatabase()
+    cursor = conn.cursor()
+    cursor.execute(sqlcode, data)
+    conn.commit()
+    return cursor.lastrowid
+
+def InsertPeriod(periodClass, nextID):
+    # Validation
+    if type(periodClass) is not Period:
+        raise TypeError("Is not a period class")
+    if type(nextID) is not int:
+        raise TypeError("Next id isn't an int")
+
+    sqlcode = """INSERT INTO periods(nextID, subject, teacher, classroom) VALUES (?,?,?,?)"""
+    data = (nextID, periodClass.subject, periodClass.teacher, periodClass.classroom)
+
+    conn = connectToDatabase()
+    cursor = conn.cursor()
+    cursor.execute(sqlcode, data)
+    conn.commit()
+    return cursor.lastrowid
+
 def SelectAllRowsFromTable(table):
     # Validation
     if type(table) is not str:
@@ -38,10 +69,22 @@ def SelectAllRowsFromTable(table):
 
     conn = connectToDatabase()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM ?", table)
+    cursor.execute("SELECT * FROM " + table)
     rows = cursor.fetchall()
     return rows
 
-rows = SelectAllRowsFromTable("collegeupdates")
-for row in rows:
-    print(row)
+def SelectAllAssignmentsFromUser(userID):
+    # Validation
+    if type(userID) is not int:
+        raise TypeError("User id is not an int")
+    
+    conn = connectToDatabase()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM assignments WHERE userID=?", (userID,))
+    
+    rows = cursor.fetchall()
+    return rows
+
+period = Period("Maths", "Joe", "LB1")
+print(InsertPeriod(period, 0))
